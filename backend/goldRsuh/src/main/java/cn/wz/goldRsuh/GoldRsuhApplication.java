ecd6a4c8f44408af86dbd0f3198035df68a1cdf7
zhiming <zhiming@ecquaria.com>
2024-08-02 10:22:17 +0800

    init springboot

---
+++ backend/goldRsuh/src/main/java/cn/wz/goldRsuh/GoldRsuhApplication.java
@@ -0,0 +1,13 @@
+package cn.wz.goldRsuh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldRsuhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoldRsuhApplication.class, args);
+	}
+
+}
---
+++ backend/goldRsuh/src/test/java/cn/wz/goldRsuh/GoldRsuhApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.wz.goldRsuh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldRsuhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
