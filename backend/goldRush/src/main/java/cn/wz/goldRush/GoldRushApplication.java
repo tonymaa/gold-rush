edb3ac220d415739640df4636e64079f3ec7891c
zhiming <zhiming@ecquaria.com>
2024-08-02 10:30:50 +0800

    init springboot

--- backend/goldRush/src/main/java/cn/wz/goldRush/GoldRushApplication.java
+++ backend/goldRush/src/main/java/cn/wz/goldRush/GoldRushApplication.java
@@ -1,13 +1,13 @@
-package cn.wz.goldRsuh;
+package cn.wz.goldRush;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GoldRsuhApplication {
+public class GoldRushApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GoldRsuhApplication.class, args);
+		SpringApplication.run(GoldRushApplication.class, args);
 	}
 
 }
