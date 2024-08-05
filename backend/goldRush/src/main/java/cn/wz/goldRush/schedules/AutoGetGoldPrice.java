0c3814b18846a3e6115aa0e5fa27bf92e94c607e
zhiming <zhiming@ecquaria.com>
2024-08-05 14:10:12 +0800

    feat: gold price

--- backend/goldRush/src/main/java/cn/wz/goldRush/schedules/AutoGetGoldPrice.java
+++ backend/goldRush/src/main/java/cn/wz/goldRush/schedules/AutoGetGoldPrice.java
@@ -4,12 +4,15 @@ package cn.wz.goldRush.schedules;
 import cn.wz.goldRush.entity.GoldPrice;
 import cn.wz.goldRush.respository.GoldPriceJpaRepository;
 import cn.wz.goldRush.service.GoldService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AutoGetGoldPrice {
+    private  static Logger logger = LoggerFactory.getLogger(AutoGetGoldPrice.class);
 
     @Autowired
     GoldService goldService;
@@ -17,7 +20,7 @@ public class AutoGetGoldPrice {
     @Autowired
     GoldPriceJpaRepository goldPriceJpaRepository;
 
-    @Scheduled(cron = "0/10 * * * * ?")
+    @Scheduled(cron = "*/10 * * * * ?")
     public void start(){
         GoldPrice goldPrice = goldService.getCnBankGoldPrice();
         goldPriceJpaRepository.save(goldPrice);
