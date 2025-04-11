579d612cdee33af971b7c6956c41f770e6b3b3b8
zhiming <zhiming@ecquaria.com>
2025-04-11 15:58:38 +0800

    fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ConfigProvider } from 'antd';
+import {ConfigProvider, theme} from 'antd';
 import zhCN from 'antd/locale/zh_CN';
 import GoldRecordPage from './pages/GoldRecordPage';
 import './App.css';
@@ -8,6 +8,7 @@ const App: React.FC = () => {
   return (
     <ConfigProvider
         locale={zhCN}
+        theme={{ algorithm: theme.darkAlgorithm }}
     >
       <GoldRecordPage />
     </ConfigProvider>
