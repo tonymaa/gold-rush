cead16941d3be25dbeb7d73a39baf8dc84edec53
zhiming <zhiming@ecquaria.com>
2025-04-14 16:53:45 +0800

    fix

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Start Spring Boot in the background
-java -jar /app/app.jar &
+java -jar -Duser.timezone=Asia/Shanghai /app/app.jar &
 
 # Start Nginx in the foreground
 nginx -g 'daemon off;' 
